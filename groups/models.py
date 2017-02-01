5b9d01c29e85245a17770ecb98d4f0249b22791f
Jayden Spencer <jaydenspencer1@gmail.com>
2017-02-01 10:36:25 +0800

    groups and user models

--- groups/models.py
+++ groups/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+#--GROUPS TABLE
+class Groups(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.TextField(max_length=255)
+	created_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
--- users/models.py
+++ users/models.py
@@ -1,18 +1,23 @@
 from __future__ import unicode_literals
 from django.db import models
 
-#--USERS TABLE
-
 #--Users - Roles
-class Users_Roles(models.Model):
-    level = models.CharField(max_length=10)
-    description = models.CharField(max_length=255)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+class Roles(models.Model):
+	level = models.CharField(max_length=10)
+	description = models.TextField(max_length=255)
+	created_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.level
+
 #--Users - Main
 class Users(models.Model):
-    name = models.CharField(max_length=20)
-    comments = models.CharField(max_length=255)
-    role = models.ForeignKey(Users_Roles)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+	name = models.CharField(max_length=20)
+	comments = models.TextField(max_length=255)
+	role_id = models.ForeignKey(Roles)
+	created_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
