2ba4e8cd9ac605e3db96ca5c0f4c3e932b5c20fb
Jayden Spencer <jaydenspencer1@gmail.com>
2017-02-02 14:50:16 +0800

    add viewer app and index home .html and nav.html

--- viewer/urls.py
+++ viewer/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
 	2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'^admin/', admin.site.urls),
-	url(r'^viewer/$'),
-	url(r'^$', views.home, name='index'),
+	url(r'^$', views.home, name='view-index'),
 ]
