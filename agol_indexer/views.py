0879579648411f9de12c29f42cb6e9cbb4e48356
Jayden Spencer <jaydenspencer1@gmail.com>
2017-02-02 17:46:52 +0800

    viewer templates and and blocks to so that index is primary location now need to duplicate

--- agol_indexer/views.py
+++ agol_indexer/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
 def index_home(request):
-	return render(request, 'index.html')
+	return render(request, 'home_search.html')
