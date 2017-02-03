49c7782d20708c85415591963c7bb5fa454203ee
Jayden Spencer <jaydenspencer1@gmail.com>
2017-02-03 12:36:11 +0800

    search button, search filtering for layer sources

--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.core import serializers
 from layer_source.models import Layer_Source
 
 def viewer_home(request):
@@ -15,8 +16,20 @@ def viewer_groups(request):
 
 def viewer_layer_source(request):
 	location = ['Layer Source']
-	layer_source = Layer_Source.objects.all()
-	return render(request, 'viewer_list.html', {'location': location, 'items': layer_source})
+	layer_source_all = Layer_Source.objects.all()
+	layer_source = None
+
+	if 'name' in request.GET:
+		name = request.GET['name']
+		if not name:
+			layer_source = layer_source_all
+		else:
+			layer_source = Layer_Source.objects.filter(name__icontains=name)
+	else:
+		layer_source = layer_source_all
+	print layer_source
+	search_data = serializers.serialize("json", layer_source_all)
+	return render(request, 'viewer_list.html', {'location': location, 'items': layer_source, 'search_data': search_data})
 
 def viewer_mxd(request):
 	location = ['MXD']
