569cdc409cb8ea50a6349999e4558d317451ed56
Ashley Felton <ashley@ropable.com>
2017-02-06 14:49:01 +0800

    Added django-confy to reqs, alter settings to obtain config from env variables.

--- agol_indexer/wsgi.py
+++ agol_indexer/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+import confy
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+confy.read_environment_file('.env')  # Must precede dj_static imports.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agol_indexer.settings")
 
 application = get_wsgi_application()
