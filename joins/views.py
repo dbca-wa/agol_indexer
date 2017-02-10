f737a67480734232871fcc078257bbab355260bd
Jayden Spencer <jaydenspencer1@gmail.com>
2017-02-10 10:38:22 +0800

    url deletion and get change way paramaters work to get joins

--- joins/views.py
+++ joins/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from webmap.models import Webmap_Item, Webmap, Webmap_App
 from groups.models import Group
 from agol.models import AGOL_Item
@@ -7,20 +7,21 @@ from agol.models import AGOL_Item
 
 # Create your views here.
 def joins_group(request):
-	group = None
-	agol_list = None
-	webmap_list = None
-	webmap_app_list = None
+	groups_list = Group.objects.values('id', 'name').order_by('name')
 
-	if 'group_id' in request.GET:
-		group_id = request.GET['group_id']
-		if group_id:
-			group = Group.objects.get(id=group_id)
+	return render(request, 'joins_index.html', {
+		'type': 'Group',
+		'selected_item': None,
+		'item_list': groups_list,
+	})
 
-			agol_list = AGOL_Item.objects.all().exclude(id__in=group.agol.all())
-			webmap_list = Webmap.objects.all().exclude(id__in=group.webmap.all())
-			webmap_app_list = Webmap_App.objects.all().exclude(id__in=group.webmap_app.all())
-		
+def joins_getGroup(request, id_a):
+	group = get_object_or_404(Group, id=id_a)
+
+	agol_list = AGOL_Item.objects.all().exclude(id__in=group.agol.all())
+	webmap_list = Webmap.objects.all().exclude(id__in=group.webmap.all())
+	webmap_app_list = Webmap_App.objects.all().exclude(id__in=group.webmap_app.all())
+
 	groups_list = Group.objects.values('id', 'name').order_by('name')
 
 	return render(request, 'joins_index.html', {
@@ -32,19 +33,21 @@ def joins_group(request):
 		'webmap_app_list': webmap_app_list,
 	})
 
-def joins_webmapitems(request):
-	wmi = None
-	agol_list = None
-	webmap_list = None
+def joins_webmapitems(request):		
+	wmi_list = Webmap_Item.objects.values('id', 'name').order_by('name')
 
-	if 'wmi_id' in request.GET:
-		wmi_id = request.GET['wmi_id']
-		if wmi_id:
-			wmi = Webmap_Item.objects.get(id=wmi_id)
+	return render(request, 'joins_index.html', {
+		'type': 'Webmap Item',
+		'selected_item': None,
+		'item_list': wmi_list,
+	})
 
-			agol_list = AGOL_Item.objects.all().exclude(id__in=wmi.agol.all())
-			webmap_list = Webmap.objects.all().exclude(id__in=wmi.webmap.all())
-		
+def joins_getWebmapitems(request, id_a):
+	wmi = get_object_or_404(Webmap_Item, id=id_a)
+
+	agol_list = AGOL_Item.objects.all().exclude(id__in=wmi.agol.all())
+	webmap_list = Webmap.objects.all().exclude(id__in=wmi.webmap.all())
+
 	wmi_list = Webmap_Item.objects.values('id', 'name').order_by('name')
 
 	return render(request, 'joins_index.html', {
@@ -61,7 +64,7 @@ def group_agol_delete(request, id_a, id_b):
 
 	group.agol.remove(agol)
 
-	return HttpResponseRedirect(reverse('joins_groups', group_id=id_a))
+	return HttpResponseRedirect(reverse_lazy('joins_groups', args=(id_a)))
 
 def group_webmap_delete(request, id_a, id_b):
 	group = get_object_or_404(Group, id=id_a)
@@ -69,7 +72,7 @@ def group_webmap_delete(request, id_a, id_b):
 
 	group.webmap.remove(webmap)
 
-	return HttpResponseRedirect(reverse('joins_groups', group_id=id_a))
+	return HttpResponseRedirect(reverse_lazy('joins_groups', args=(id_a)))
 
 def group_webmap_app_delete(request, id_a, id_b):
 	group = get_object_or_404(Group, id=id_a)
@@ -77,7 +80,7 @@ def group_webmap_app_delete(request, id_a, id_b):
 
 	group.webmap_app.remove(webmap_app)
 
-	return HttpResponseRedirect(reverse('joins_groups', group_id=id_a))
+	return HttpResponseRedirect(reverse_lazy('joins_webmapitems', args=(id_a)))
 
 def webmapitem_agol_delete(request, id_a, id_b):
 	webmap_item = get_object_or_404(Webmap_Item, id=id_a)
@@ -85,7 +88,7 @@ def webmapitem_agol_delete(request, id_a, id_b):
 
 	webmap_item.agol.remove(agol)
 
-	return HttpResponseRedirect(reverse('joins_webmapitems', wmi_id=id_a))
+	return HttpResponseRedirect(reverse_lazy('joins_webmapitems', args=(id_a)))
 
 def webmapitem_webmap_delete(request, id_a, id_b):
 	webmap_item = get_object_or_404(Webmap_Item, id=id_a)
@@ -93,4 +96,4 @@ def webmapitem_webmap_delete(request, id_a, id_b):
 
 	webmap_item.webmap.remove(webmap)
 
-	return HttpResponseRedirect(reverse('joins_webmapitems', wmi_id=id_a))
+	return HttpResponseRedirect(reverse_lazy('joins_webmapitems', args=(id_a)))
