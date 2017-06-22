846c39b68c7504b40bfefb592c46b5a1d9378f79
Kelly Thomas <kelly.thomas@hotmail.com.au>
2017-06-22 18:03:30 +0800

    Add url field to Webmap card

--- webmap/forms.py
+++ webmap/forms.py
@@ -4,6 +4,7 @@ class Webmap_AdminForm(forms.ModelForm):
 	class Meta:
 		widgets = {
 			'name': forms.TextInput(attrs={'size': 50}),
+			'url': forms.TextInput(attrs={'size': 80}),
 			'purpose': forms.TextInput(attrs={'size': 80})
 		}
 
