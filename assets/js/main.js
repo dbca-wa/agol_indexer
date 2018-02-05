2328d9601e28bb97ec4983bbda8104dcdda47ae7
Kelly Thomas <kelly.thomas@dbca.wa.gov.au>
2018-02-05 14:18:54 +0800

    auto expand item when showinf a single item page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,89 @@
 $(document).ready(function() {
 
-	$('#cal').calendar({
-		today: true,
-		initialDate: null,
-		monthFirst: false,
-		 ampm: false,
-		formatter: {
-			date: function (date, settings) {
-				if (!date) return '';
-				var day = date.getDate();
-				var month = date.getMonth() + 1;
-				var year = date.getFullYear();
-				return day + '/' + month + '/' + year;
-			}
-		}
-	});
+    $('#cal').calendar({
+        today: true,
+        initialDate: null,
+        monthFirst: false,
+         ampm: false,
+        formatter: {
+            date: function (date, settings) {
+                if (!date) return '';
+                var day = date.getDate();
+                var month = date.getMonth() + 1;
+                var year = date.getFullYear();
+                return day + '/' + month + '/' + year;
+            }
+        }
+    });
 
-	$('.ui.dropdown').dropdown({
-		fullTextSearch: true
-	})
+    $('.ui.dropdown').dropdown({
+        fullTextSearch: true
+    })
 
-	$('.ui.dropdown.search-dropdown').dropdown({
-		fullTextSearch: true,
-		allowAdditions: true,
-		hideAdditions: false,
-		message: {
-			addResult: 'Search <b>{term}</b>'
-		},
-		onChange: function(value, text, $selectedItem) {
-			if (value) {
-				$('.viewer-button').removeClass('disabled')
-			}
-		}
-	})
+    $('.ui.dropdown.search-dropdown').dropdown({
+        fullTextSearch: true,
+        allowAdditions: true,
+        hideAdditions: false,
+        message: {
+            addResult: 'Search <b>{term}</b>'
+        },
+        onChange: function(value, text, $selectedItem) {
+            if (value) {
+                $('.viewer-button').removeClass('disabled')
+            }
+        }
+    })
 
-	$('.ui.dropdown.join-item-dropdown').dropdown({
-		fullTextSearch: true,
-		onChange: function(value, text, $selectedItem) {
-			if (value) {
-				$('.join-select-button').removeClass('disabled')
-			}
-		}
-	})
+    $('.ui.dropdown.join-item-dropdown').dropdown({
+        fullTextSearch: true,
+        onChange: function(value, text, $selectedItem) {
+            if (value) {
+                $('.join-select-button').removeClass('disabled')
+            }
+        }
+    })
 
-	$('.ui.dropdown.add-list').dropdown({
-		fullTextSearch: true,
-		onChange: function(value, text, $selectedItem) {
-			if (value) {
-				$(this).parent().parent().find('.join-add-button').removeClass('disabled')
-			}
-		}
-	})
+    $('.ui.dropdown.add-list').dropdown({
+        fullTextSearch: true,
+        onChange: function(value, text, $selectedItem) {
+            if (value) {
+                $(this).parent().parent().find('.join-add-button').removeClass('disabled')
+            }
+        }
+    })
 
-	if (window.location.href.includes('search') || window.location.href.includes('dependencies')) {
-		console.log('find')
-		$('.ui.card').each(function(index) {
-			console.log(index)
-			$(this).find('.card-accordion').removeClass('accordion')
-		})
-	}
+    if (window.location.href.includes('search') || window.location.href.includes('dependencies')) {
+        console.log('find')
+        $('.ui.card').each(function(index) {
+            console.log(index)
+            $(this).find('.card-accordion').removeClass('accordion')
+        })
+    }
 
-	$('.ui.accordion').accordion();
+    $('.ui.accordion').accordion();
 
-	$('.activating.element').popup();
+    $('.activating.element').popup();
 
-	$('.link-list').each(function(i, val) {
-		if($(this).find('li').length == 0) {
-			$(this).remove()
-		}
-	})
+    $('.link-list').each(function(i, val) {
+        if($(this).find('li').length == 0) {
+            $(this).remove()
+        }
+    })
 
-	$('.card-accordion').click(function() {
-		if ($(this).find('i').hasClass('down')) {
-			$(this).find('i').addClass('up')
-			$(this).find('i').removeClass('down')
-			$(this).find('.accord-exp-min').text('minimise')
-		} else {
-			$(this).find('i').addClass('down')
-			$(this).find('i').removeClass('up')
-			$(this).find('.accord-exp-min').text('expand')
-		}
-	})
+    $('.card-accordion').click(function() {
+        if ($(this).find('i').hasClass('down')) {
+            $(this).find('i').addClass('up')
+            $(this).find('i').removeClass('down')
+            $(this).find('.accord-exp-min').text('minimise')
+        } else {
+            $(this).find('i').addClass('down')
+            $(this).find('i').removeClass('up')
+            $(this).find('.accord-exp-min').text('expand')
+        }
+    })
+
+    var expandMinLinks = $("div.expand-min-item")
+    if (expandMinLinks.length == 1)    {
+        expandMinLinks[0].click();
+    }
 })
